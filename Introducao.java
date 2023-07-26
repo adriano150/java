ef555ffcb9087dfe8d5875ecef9088019090fd25
adriano150 <115594802+adriano150@users.noreply.github.com>
2023-07-26 19:19:12 -0300

    Add files via upload

---
+++ Introducao.java
@@ -0,0 +1,31 @@
+package Variaveis;
+
+public class Introducao {
+
+	public static void main(String[] args) {
+		// Variáveis locais.
+		
+		int lapis = 0;
+		float leite = 4, agua = 5;
+		float cafe = 4.5f;
+		
+		agua = cafe;
+		
+		double x = 10.35784;
+		
+		System.out.println(x);
+		System.out.printf("%.2f%n", x);
+		System.out.printf("%.4f%n", x);
+		
+		System.out.println("RESULTADO = " + x + " METROS");
+		System.out.printf("RESULTADO = %.2f metros%n", x);
+		
+		String nome = "Maria";
+		int idade = 31;
+		double renda = 4000.0;
+		System.out.printf("%s tem %d anos e ganha R$ %.2f reais%n", nome, idade, renda);
+		
+		
+	}
+
+}
